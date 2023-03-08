964b2b0be8790dc3ef1b9058ef91af364a3d1df2
Maewa <maewa.dev@gmail.com>
2023-03-08 21:02:30 +0100

    installations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  name: string = 'maewa';
+
+  showName() {
+    console.log(this.name);
+  }
 }
