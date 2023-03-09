97ebc04af0d7503a9e091ee18c6ba83f28655e9c
Maewa <maewa.dev@gmail.com>
2023-03-09 16:32:56 +0100

    primeNg elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import {ButtonModule} from 'primeng/button';
   imports: [
     BrowserModule,
     ButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
